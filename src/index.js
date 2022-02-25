9343d314179dad342a72aaac946c3d220e96f0ae
Aditya Kumar Panda <adityapanda1001@gmail.com>
2022-02-25 14:53:04 +0530

    Updating codes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import KpointPlayer from './components'
+import KpointPlayer from './lib/components/KpointPlayer'
 
 ReactDOM.render(
   <React.StrictMode>
